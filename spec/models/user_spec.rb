7fbba3b4c0032a3ee2c6f58a686df67fdf3528a5
ky004689 <ky004689@etu.unice.fr>
2012-10-12 06:41:02 -0700

    Passing test name/mail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end 
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+ 
+  it { should be_valid }
+  describe "Nom non present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 end
