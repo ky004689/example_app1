26b107171bb3525952c35dbcd028dee219c2fdd4
ky004689 <ky004689@etu.unice.fr>
2012-10-12 07:09:48 -0700

    Regex mail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,4 +37,25 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  describe "Le format de l'email est invalide!" do
+   it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end      
+    end
+  end
+
+  describe "Le format de l'email est valide" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn zk000133@etu.unice.fr]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end      
+    end
+  end
 end
