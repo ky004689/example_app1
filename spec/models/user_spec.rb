6a17a1afab38874ffbf5807ec6e9eb5312f1a625
ky004689 <ky004689@etu.unice.fr>
2012-10-12 07:18:11 -0700

    Unique email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,4 +58,15 @@ describe User do
       end      
     end
   end
+
+  describe "L'adresse email est deja prise" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
 end
