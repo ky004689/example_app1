bc8155724726d4d6010ad66726d10d35c3ac9c31
ky004689 <ky004689@etu.unice.fr>
2012-11-10 18:50:13 +0100

    Tests + factoryGirls

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	  factory :user do
+		  name     "ZK"
+		  email    "zk@example.com"
+		  password "foobar"
+		  password_confirmation "foobar"
+	  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
-describe "Users" do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_path
-      response.status.should be(200)
-    end
-  end
+describe "User pages" do
+	subject { page }
+
+	describe "signup page" do
+		before { visit signup_path }
+		it { should have_selector('h1',    text: 'Sign up') }
+		it { should have_selector('title', text: 'Sign up') }
+	end
+
+	describe "profile page" do
+		let(:user) { FactoryGirl.create(:user) }
+	 	before { visit user_path(user) }
+	     	it { should have_selector('h1',    text: user.name) }
+	     	it { should have_selector('title', text: user.name) }
+	end
 end
