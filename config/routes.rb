31ff0fda1a568c280afc71a8d4f99328fc0b7d73
ky004689 <ky004689@etu.unice.fr>
2012-11-10 23:23:53 +0100

    Chp 8.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ExampleApp::Application.routes.draw do
   resources :microposts
-
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
+
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
@@ -13,6 +14,8 @@ ExampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
