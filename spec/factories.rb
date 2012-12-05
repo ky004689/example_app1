131aa27cbdd96e04eba5d83c8442b1e2020f2b00
ky004689 <ky004689@etu.unice.fr>
2012-12-05 18:38:28 +0100

    chap 9.1.3

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
-	  factory :user do
-		  name     "Zerouali Khalid"
-		  email    "zero.khalid@gmail.com"
-		  password "foobar"
-		  password_confirmation "foobar"
-	  end
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
 end
+
