2197df8819f0974d8604741645a17757cdc0839c
ky004689 <ky004689@etu.unice.fr>
2012-12-05 19:58:15 +0100

    test

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(:version => 20121110215158) do
 
-  create_table "microposts", :force => true do |t|
+  create_table "microposts", :force => true do |t| 
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
